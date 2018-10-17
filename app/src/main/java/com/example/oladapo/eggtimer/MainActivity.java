6fcb628c3627d039a1a90abfc593e24f3318f413
Anthony <you@example.comcls>
2018-10-17 08:44:23 +0000

    added code for seek bar and text view

--- app/src/main/java/com/example/oladapo/eggtimer/MainActivity.java
+++ app/src/main/java/com/example/oladapo/eggtimer/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.oladapo.eggtimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar timerSeekBar = findViewById(R.id.timerSeekBar);
+        final TextView timerTextView = findViewById(R.id.timerTextView);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                int minutes = progress / 60;
+                int seconds = progress - minutes * 60;
+
+                String secondString = Integer.toString(seconds);
+
+                if (secondString == "0") {
+
+                    secondString = "00";
+
+                }
+
+                timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
