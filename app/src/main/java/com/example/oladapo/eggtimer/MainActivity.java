e51db78ea10341d01d89fbcc54f65d86da5afc84
Anthony <you@example.comcls>
2018-10-17 09:02:22 +0000

    added code for button

--- app/src/main/java/com/example/oladapo/eggtimer/MainActivity.java
+++ app/src/main/java/com/example/oladapo/eggtimer/MainActivity.java
@@ -1,19 +1,62 @@
 package com.example.oladapo.eggtimer;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar timerSeekBar;
+    TextView timerTextView;
+
+    public void updateTimer(int secondsLeft) {
+
+        int minutes = (int) secondsLeft / 60;
+        int seconds = secondsLeft - minutes * 60;
+
+        String secondString = Integer.toString(seconds);
+
+        if (secondString == "0") {
+
+            secondString = "00";
+
+        }
+
+        timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+
+    }
+
+     public void controlTimer(View view) {
+
+         new CountDownTimer(timerSeekBar.getProgress() * 1000, 1000){
+
+
+             @Override
+             public void onTick(long millisUntilFinished) {
+
+                    updateTimer((int) millisUntilFinished / 1000);
+
+             }
+
+             @Override
+             public void onFinish() {
+
+             }
+         };
+
+     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SeekBar timerSeekBar = findViewById(R.id.timerSeekBar);
-        final TextView timerTextView = findViewById(R.id.timerTextView);
+        timerSeekBar = findViewById(R.id.timerSeekBar);
+        timerTextView = findViewById(R.id.timerTextView);
 
         timerSeekBar.setMax(600);
         timerSeekBar.setProgress(30);
@@ -22,18 +65,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
-                int minutes = progress / 60;
-                int seconds = progress - minutes * 60;
-
-                String secondString = Integer.toString(seconds);
-
-                if (secondString == "0") {
-
-                    secondString = "00";
-
-                }
-
-                timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+                updateTimer(progress);
 
             }
 
